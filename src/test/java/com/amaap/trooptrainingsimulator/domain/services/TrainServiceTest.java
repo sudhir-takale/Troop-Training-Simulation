72f2267ecdd7a4dbd6a568011cfbb8af60d26860
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-24 21:00:30 +0530

    added test for trainservice

--- src/test/java/com/amaap/trooptrainingsimulator/domain/services/TrainServiceTest.java
+++ src/test/java/com/amaap/trooptrainingsimulator/domain/services/TrainServiceTest.java
@@ -2,6 +2,7 @@ package com.amaap.trooptrainingsimulator.domain.services;
 
 import com.amaap.trooptrainingsimulator.domain.models.Barrack;
 import com.amaap.trooptrainingsimulator.models.Archers;
+import com.amaap.trooptrainingsimulator.models.Barbarian;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,11 +26,27 @@ class TrainServiceTest {
 
     @Test
     void shouldBeAbleToTrainTheTroop() {
+//        Act & Assert
+        boolean result = trainService.trainTroops(barrack);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void shouldRemoveTroopFromTheListAfterTraining() {
 //        Act
         boolean result = trainService.trainTroops(barrack);
-//        Assert
+        Assertions.assertEquals(0, barrack.getTroops().size());
+    }
+    @Test
+    void shouldAbleToTrainAllTroopsRegardlessOfType() {
+//        Arrange
+        barrack.getTroops().add(new Archers());
+        barrack.getTroops().add(new Barbarian());
+        // Act
+        boolean result = trainService.trainTroops(barrack);
+        // Assert
         Assertions.assertTrue(result);
-
+        Assertions.assertEquals(0, barrack.getTroops().size());
     }
 
 }
