f4163ee28924bd3281b5a0a9738acb3e19cd77e4
Sudhir Takale <sudhirtakale99@gmail.com>
2024-05-24 11:06:11 +0530

    added testcases for database

---
+++ src/test/java/com/amaap/trooptrainingsimulator/repository/db/InMemoryDBTest.java
@@ -0,0 +1,101 @@
+package com.amaap.trooptrainingsimulator.repository.db;
+
+import com.amaap.trooptrainingsimulator.domain.model.Troop;
+import com.amaap.trooptrainingsimulator.domain.model.TroopType;
+import com.amaap.trooptrainingsimulator.domain.model.exception.InvalidTroopParamsException;
+import com.amaap.trooptrainingsimulator.repository.db.exception.TroopNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+class InMemoryDBTest {
+    InMemoryDB inMemoryDB = new FakeInMemoryDatabase();
+
+    @Test
+    void add() throws InvalidTroopParamsException {
+        // arrange
+        Troop added = new Troop(1, 3, 10);
+
+        // act
+        Troop actual = inMemoryDB.add(added);
+
+        // assert
+        assertEquals(added, actual);
+    }
+
+    @Test
+    void get() throws InvalidTroopParamsException, TroopNotFoundException {
+        // arrange
+        Troop created = inMemoryDB.add(new Troop(1, 3, 10));
+
+        // act
+        Troop troop = inMemoryDB.get(1);
+
+        // act
+        assertEquals(1, troop.getId());
+    }
+
+    @Test
+    void getAllTroops() throws InvalidTroopParamsException {
+        // arrange
+        inMemoryDB.add(new Troop(1, 3, 10));
+        inMemoryDB.add(new Troop(2, 3, 10));
+        inMemoryDB.add(new Troop(3, 3, 10));
+
+        // act
+        List<Troop> allTroops = inMemoryDB.getAllTroops();
+
+        // assert
+        assertEquals(3, allTroops.size());
+    }
+
+
+    @Test
+    void getBarracks() throws InvalidTroopParamsException {
+        // arrange
+        inMemoryDB.insert(new Troop(1, 3, 10));
+        inMemoryDB.insert(new Troop(1, 3, 10));
+        inMemoryDB.insert(new Troop(1, 3, 10));
+
+        // act
+        Queue<Troop> barracks = inMemoryDB.getBarracks();
+
+        // assert
+        assertEquals(3, barracks.size());
+    }
+
+    @Test
+    void updateTrainedTroop() throws InvalidTroopParamsException {
+        // arrange
+        Troop troop = new Troop(1, 3, 10);
+        inMemoryDB.insert(troop);
+
+        // act
+        boolean b = inMemoryDB.updateTrainedTroop(TroopType.BARBARIAN);
+
+        // assert
+        assertTrue(b);
+
+    }
+
+    @Test
+    void getTrainedTroops() {
+        // arrange
+        inMemoryDB.updateTrainedTroop(TroopType.BARBARIAN);
+        inMemoryDB.updateTrainedTroop(TroopType.BARBARIAN);
+        inMemoryDB.updateTrainedTroop(TroopType.BARBARIAN);
+
+        // act
+        Map<TroopType, Integer> trainedTroops = inMemoryDB.getTrainedTroops();
+
+        // assert
+        assertEquals(1, trainedTroops.size());
+
+
+    }
+}
